a7d7725c15b56f5bd02cfb8f3b1ea4789d45a01b
pranesh-S-S <praneshsrinivasan74@outlook.com>
2026-05-17 11:56:17 +0530

    Halved mobile scroll distance for faster landing and amplified text lift effect

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,7 +27,7 @@ export default function Hero() {
   // All useTransform hooks called unconditionally at top level
   const frameIndex = useTransform(scrollYProgress, [0, 1], [0, FRAME_COUNT - 1])
   const textOpacity = useTransform(scrollYProgress, [0.15, 0.35, 0.7, 1], [0, 1, 1, 1])
-  const textY = useTransform(scrollYProgress, [0.15, 0.35, 0.7, 0.85], [60, 0, 0, -80])
+  const textY = useTransform(scrollYProgress, [0.15, 0.35, 0.7, 1], [60, 0, 0, -200])
   const badgeOpacity = useTransform(scrollYProgress, [0.1, 0.25], [0, 1])
   const ctaOpacity = useTransform(scrollYProgress, [0.3, 0.45, 0.7, 1], [0, 1, 1, 1])
   const bigTextY = useTransform(scrollYProgress, [0.0, 0.9], [100, -250])
@@ -107,7 +107,7 @@ export default function Hero() {
   const isLoaded = images.length > 0
 
   return (
-    <div ref={containerRef} className="relative h-[400vh]">
+    <div ref={containerRef} className="relative h-[250vh] md:h-[400vh]">
       {/* Loading screen */}
       {!isLoaded && (
         <div className="fixed inset-0 z-[200] bg-[#050a12] flex flex-col items-center justify-center gap-6">
